f9dd382097450954b23af338bfc5d5d29b787442
Brandon Browning <brandonbrowning@live.com>
2014-09-09 23:34:44 -0400

    Fixed up sample?

--- c++/sample/sample.cpp
+++ c++/sample/sample.cpp
@@ -1,20 +1,18 @@
 #include "sample.hpp"
 
-void increment_problem(std::istream& input, std::ostream& output)
-{
-  int x;
+void increment_problem(std::istream& input, std::ostream& output) {
+    int x;
 
-  for (int i = 0; i < 10; ++i)
-  {
-    input >> x;
-    std::cout << x << std::endl;
-    if (x == 0)
-      break;
-    output << (x + 1) << std::endl;
-  }
+    for (int i = 0; i < 10; ++i) {
+        input >> x;
+
+        if (x == 0)
+            break;
+
+        output << increment_algorithm(x) << std::endl;
+    }
 }
 
-int increment_algorithm(int value)
-{
-  return value + 1;
+int increment_algorithm(int value) {
+    return value + 1;
 }
