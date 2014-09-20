81e74152e39a043e864a6b1770495049b68a0bce
Alex Addy <alex.addy@gmail.com>
2014-09-19 20:21:35 -0400

    Reimplemented a basic BFSLength function

--- c++/Breadth First Search/BFS.cpp
+++ c++/Breadth First Search/BFS.cpp
@@ -1,37 +1,43 @@
-#include<iostream>
 #include<vector>
 
 using namespace std;
 
-bool success = true;
-
 struct Node {
-	vector<Node*> children;
-	char name;
+	vector<Node*> connections;
+	int value;
 	bool visited;
 
-	Node() {
+	Node(int val) {
 		this->visited = false;
+		this->value = val;
 	}
-
 };
 
-vector<Node*> BFSTraversal (vector<Node*> graph, int start = 0) {
-	
-	vector<Node*> order;
+/*
+ * arguments are start and end nodes
+ * returns length of path to end node
+ * returns -1 if end node is not found
+ */
+int BFSLength (Node* start, Node* end) {
+	Node* sentinel = NULL;
+	int length = 0;
 	vector<Node*> queue;
 
-	queue.push_back(graph[start]);
-	queue[0]->visited = true;
+	queue.push(start);
+	start->visited = true;
 
 	while(!queue.empty()) {
 		
-		Node* cur_node = queue.front();
-		queue.erase(queue.begin());
-		order.push_back(cur_node);
+		Node* cur_node = queue.pop();
+		if (cur_node == end) {
+			return length;
+		}
+		if (sentinel == cur_node) {
+			length++;
+			sentinel = cur_node->connections.back();
+		}
 
 		for(int i = 0; i < cur_node->children.size(); i++) {
-			
 			Node *child = cur_node->children[i];
 			
 			if(!child->visited) {
@@ -40,119 +46,5 @@ vector<Node*> BFSTraversal (vector<Node*> graph, int start = 0) {
 			}
 		}
 	}
-	return order;
-}
-
-bool testOne(bool printDebug) {
-	Node* tree[7];
-
-	for (int i = 0; i < 7; i++) {
-		tree[i] = new Node();
-		tree[i]->name = 'A' + i;
-	}
-
-	vector<Node*> graph;
-	tree[0]->children.push_back(tree[1]);
-	tree[0]->children.push_back(tree[2]);
-
-	tree[1]->children.push_back(tree[0]);
-	tree[1]->children.push_back(tree[3]);
-	tree[1]->children.push_back(tree[4]);
-
-	tree[2]->children.push_back(tree[0]);
-	tree[2]->children.push_back(tree[5]);
-	tree[2]->children.push_back(tree[6]);
-
-	tree[3]->children.push_back(tree[1]);
-
-	tree[4]->children.push_back(tree[1]);
-
-	tree[5]->children.push_back(tree[2]);
-
-	tree[6]->children.push_back(tree[2]);
-	
-	for(int i = 0; i < 7; i++) {
-		graph.push_back(tree[i]);
-	}
-
-	vector<Node*> traversal = BFSTraversal(graph);
-
-	//Traversal of graph is expected to be in alphabetical oder
-	for (int i = 0; i < traversal.size(); i++) {
-		if(traversal[i]->name != ('A' + i)) {
-			success = false;
-			return false;
-		}
-	}
-
-	// We're traversing the graph again and nodes are still visited so we only get one
-	traversal = BFSTraversal(graph, 6);
-	if (traversal.size() > 1 || !traversal[0]->name == 'G') {
-		success = false;
-		return false;
-	}
-	return true;
-}
-
-bool testTwo(bool printDebug) {
-	Node* tree[7];
-
-	for (int i = 0; i < 7; i++) {
-		tree[i] = new Node();
-		tree[i]->name = 'A' + i;
-	}
-
-	vector<Node*> graph;
-	tree[0]->children.push_back(tree[1]);
-	tree[0]->children.push_back(tree[2]);
-
-	tree[1]->children.push_back(tree[0]);
-	tree[1]->children.push_back(tree[3]);
-	tree[1]->children.push_back(tree[4]);
-
-	tree[2]->children.push_back(tree[0]);
-	tree[2]->children.push_back(tree[5]);
-	tree[2]->children.push_back(tree[6]);
-
-	tree[3]->children.push_back(tree[1]);
-
-	tree[4]->children.push_back(tree[1]);
-
-	tree[5]->children.push_back(tree[2]);
-
-	tree[6]->children.push_back(tree[2]);
-
-	//tree[6]->children.push_back(tree[1]);
-	//tree[1]->children.push_back(tree[6]);
-
-	for(int i = 0; i < 7; i++) {
-		graph.push_back(tree[i]);
-	}
-
-	vector<Node*> traversal = BFSTraversal(graph, 6);
-
-	
-
-	char answer[] = {'G', 'C', 'A', 'F', 'B', 'D', 'E'};
-
-	for (int i = 0; i < 7; i++) {
-		if (answer[i] != traversal[i]->name) {
-			success = false;
-			return false;
-		}
-	}
-	return true;
-}
-
-int main() {
-	if (testOne(true) == false)
-		cout << "FAILED: Test One BFS" << endl;
-	if (testTwo(true) == false)
-		cout << "FAILED: Test Two BFS" << endl;
-	if(!success) {
-		system("pause");
-		return 1;
-	}
-	else
-		return 0;
+	return -1;
 }
